f5a4e37579a32dd9d1d1b3128b58576fd3d41e9d
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-12-30 17:32:17 +0300

    dakar

---
+++ src/main/java/com/hastycode/DakarHotel/DakarHotelApplication.java
@@ -0,0 +1,13 @@
+package com.hastycode.DakarHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DakarHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DakarHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hastycode/DakarHotel/DakarHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hastycode.DakarHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DakarHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
