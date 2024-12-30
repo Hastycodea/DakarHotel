11b8707db9562d8e56eeae6da0d79fcdffb33010
Hastycode Andreh <hastycodeandreh@gmail.com>
2024-12-30 22:47:24 +0300

    updatedEntity

---
+++ src/main/java/com/hastycode/DakarHotel/entity/Booking.java
@@ -0,0 +1,62 @@
+package com.hastycode.DakarHotel.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@Table(name = "bookings")
+public class Booking {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "check in required")
+    private LocalDate checkInDate;
+
+    @Future(message = "check out must be in future")
+    private LocalDate checkOutDate;
+
+    @Min(value = 1, message = "Number of adults must not be less than one")
+    private int numOfAdults;
+
+    @Min(value = 0, message = "Number of children must not be less than zero")
+    private int numOfChildren;
+
+    private int totalNumOfGuest;
+
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumberOfGuest() {
+        this.totalNumOfGuest = this.numOfAdults + this.numOfChildren;
+    }
+
+    @Override
+    public String toString() {
+        return "Booking{" +
+                "id=" + id +
+                ", checkInDate=" + checkInDate +
+                ", checkOutDate=" + checkOutDate +
+                ", numOfAdults=" + numOfAdults +
+                ", numOfChildren=" + numOfChildren +
+                ", totalNumOfGuest=" + totalNumOfGuest +
+                ", bookingConfirmationCode='" + bookingConfirmationCode + '\'' +
+                ", user=" + user +
+                ", room=" + room +
+                '}';
+    }
+}
---
+++ src/main/java/com/hastycode/DakarHotel/entity/Room.java
@@ -0,0 +1,14 @@
+package com.hastycode.DakarHotel.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "rooms")
+public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
